51053cdc84d446c5b3b12d0770e97275604ee6c7
jklijlllll <kellywong48357@gmail.com>
2022-09-10 03:46:26 -0600

    login page ui

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,12 +1,26 @@
 import { FunctionComponent } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login: FunctionComponent<{}> = () => {
+  let navigate = useNavigate();
   return (
-    <header className="App-header">
-      <a className="btn-spotify" href="/auth/login">
-        Login with Spotify
-      </a>
-    </header>
+    <div className="login_container">
+      <h1 className="app_title">App Title</h1>
+      <h4 className="app_description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia
+        scelerisque rhoncus.
+      </h4>
+      <button
+        className="spotify_button"
+        onClick={() => navigate("/auth/login")}
+      >
+        <img
+          className="spotify_icon"
+          src={require("../../Media/Spotify_Icon_RGB_White.png")}
+        />
+        <h4 className="login_text">Login with Spotify</h4>
+      </button>
+    </div>
   );
 };
 
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -9,13 +9,22 @@ const Home: FunctionComponent<{}> = () => {
     async function getToken() {
       const response = await fetch("/auth/token");
       const json = await response.json();
+      console.log(json.access_token);
       setToken(json.access_token);
     }
 
     getToken();
   }, []);
 
-  return <>{token === "" ? <Login /> : <WebPlayback token={token} />}</>;
+  return (
+    <div className="app_container">
+      {token === "" || token === undefined ? (
+        <Login />
+      ) : (
+        <WebPlayback token={token} />
+      )}
+    </div>
+  );
 };
 
 export default Home;
