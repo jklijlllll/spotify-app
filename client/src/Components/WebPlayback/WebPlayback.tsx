bb16ddb7a93ba769860c5bc37340a6c8581b1535
jklijlllll <kellywong48357@gmail.com>
2022-09-14 19:45:24 -0600

    volume control and position slider ui

--- client/src/Components/WebPlayback/WebPlayback.tsx
+++ client/src/Components/WebPlayback/WebPlayback.tsx
@@ -4,10 +4,13 @@ import SkipNextIcon from "@mui/icons-material/SkipNext";
 import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import PauseIcon from "@mui/icons-material/Pause";
 import axios from "axios";
-import { IconButton } from "@mui/material";
+import { Fade, IconButton, Slider, Tooltip } from "@mui/material";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 
-// TODO: add volume control
 // TODO: add device control
+// TODO: save player state on reload
 const WebPlayback: FunctionComponent<{
   token: string;
   current_track: any;
@@ -27,6 +30,10 @@ const WebPlayback: FunctionComponent<{
 }) => {
   const [player, setPlayer] = useState<any>(undefined);
   const [is_paused, setPaused] = useState(false);
+  const [volume, setVolume] = useState(50);
+  const [position, setPosition] = useState<number>(0);
+  const [duration, setDuration] = useState<number>(0);
+  const [updateTime, setUpdateTime] = useState<number>(0);
 
   useEffect(() => {
     if (token !== "") {
@@ -45,6 +52,7 @@ const WebPlayback: FunctionComponent<{
           volume: 0.5,
         });
 
+        setVolume(50);
         setPlayer(player);
 
         player.addListener("ready", (device_id: any) => {
@@ -64,6 +72,9 @@ const WebPlayback: FunctionComponent<{
 
           setTrack(state.track_window.current_track);
           setPaused(state.paused);
+          setPosition(state.position);
+          setDuration(state.duration);
+          setUpdateTime(performance.now());
 
           player.getCurrentState().then((state: any) => {
             !state ? setActive(false) : setActive(true);
@@ -73,6 +84,14 @@ const WebPlayback: FunctionComponent<{
       };
     }
   }, [token]);
+
+  const getPosition = () => {
+    if (is_paused) {
+      return position;
+    }
+    let cur_position = position + (performance.now() - updateTime) / 1000;
+    return cur_position > duration ? duration : cur_position;
+  };
 
   const headers = {
     Authorization: `Bearer ${token}`,
@@ -90,9 +109,26 @@ const WebPlayback: FunctionComponent<{
       });
   }, [deviceId]);
 
+  useEffect(() => {
+    if (!is_active) return;
+
+    player.setVolume(volume / 100);
+  }, [volume]);
+
   if (is_active) {
     return (
       <>
+        <div className="position_container">
+          <Slider
+            sx={{
+              width: "calc(100% - 20px)",
+              "& .MuiSlider-rail": {
+                width: "calc(100% + 20px)",
+                left: "-10px",
+              },
+            }}
+          />
+        </div>
         <div className="webplayback_info">
           <img
             src={current_track.album.images[1].url}
@@ -106,25 +142,65 @@ const WebPlayback: FunctionComponent<{
               {current_track.artists[0].name}
             </div>
           </div>
-        </div>
-        <div className="playback_controls">
-          <IconButton onClick={() => player!.previousTrack()}>
-            <SkipPreviousIcon fontSize="large" />
-          </IconButton>
-          <IconButton
-            onClick={() => {
-              player!.togglePlay();
-            }}
-          >
-            {!is_paused ? (
-              <PauseIcon fontSize="large" />
-            ) : (
-              <PlayArrowIcon fontSize="large" />
-            )}
-          </IconButton>
-          <IconButton onClick={() => player!.nextTrack()}>
-            <SkipNextIcon fontSize="large" />
-          </IconButton>
+          <div className="playback_controls">
+            <IconButton onClick={() => player!.previousTrack()}>
+              <SkipPreviousIcon fontSize="large" />
+            </IconButton>
+            <IconButton
+              onClick={() => {
+                player!.togglePlay();
+              }}
+            >
+              {!is_paused ? (
+                <PauseIcon fontSize="large" />
+              ) : (
+                <PlayArrowIcon fontSize="large" />
+              )}
+            </IconButton>
+            <IconButton onClick={() => player!.nextTrack()}>
+              <SkipNextIcon fontSize="large" />
+            </IconButton>
+          </div>
+          <div className="device_controls">
+            <div className="device_controls_container">
+              <Tooltip
+                title={
+                  <div className="volume_control">
+                    <Slider
+                      sx={{
+                        '& input[type="range"]': {
+                          WebkitAppearance: "slider-vertical",
+                        },
+                        color: "black",
+                        backgroundColor: "transparent",
+                        height: "280px",
+                      }}
+                      orientation="vertical"
+                      value={volume}
+                      onChange={(event: any, newValue: any) => {
+                        setVolume(newValue);
+                      }}
+                      aria-label="Temperature"
+                      valueLabelDisplay="auto"
+                    />
+                  </div>
+                }
+                arrow
+                TransitionComponent={Fade}
+                TransitionProps={{ timeout: 600 }}
+              >
+                <IconButton>
+                  {volume === 0 ? (
+                    <VolumeOffIcon fontSize="large" />
+                  ) : volume < 50 ? (
+                    <VolumeDownIcon fontSize="large" />
+                  ) : (
+                    <VolumeUpIcon fontSize="large" />
+                  )}
+                </IconButton>
+              </Tooltip>
+            </div>
+          </div>
         </div>
       </>
     );
